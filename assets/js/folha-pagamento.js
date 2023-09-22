bdf79e292d458be625013eb0bf199419d6343820
Thalisson <thalissonboth9.com>
2023-09-22 16:10:01 -0300

    alteração em folha de recibo

---
+++ assets/js/folha-pagamento.js
@@ -0,0 +1,11 @@
+const form = document.getElementById('calculo');
+const nome = document.getElementById('nome');
+const cpf = document.getElementById('cpf');
+const funcao = document.getElementById('funcao');
+const salario = document.getElementById('salario');
+const diasTrabalhados = document.getElementById('diasTrabalhados');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    /*alert(`O funcionário ${nome.value} inscrito no cpf: ${cpf.value} exercendo a função de ${funcao.value} com o salário mensal de ${salario.value} trabalhou um total de ${diasTrabalhados.value} dias.`) alert para testar se está puxando os valores certos*/
+})
