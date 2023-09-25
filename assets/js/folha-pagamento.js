ab882bebb09dca5295f64d0c9d85718ce5642783
Thalisson <thalissonboth9.com>
2023-09-25 13:47:21 -0300

    iinicio incrementação recibo

--- assets/js/folha-pagamento.js
+++ assets/js/folha-pagamento.js
@@ -7,5 +7,8 @@ const diasTrabalhados = document.getElementById('diasTrabalhados');
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
+    const valorDia = salario/30
+    const recibo = document.getElementById('recibo')
+    recibo.innerHTML = ('<p> Teste </p>')
     /*alert(`O funcionário ${nome.value} inscrito no cpf: ${cpf.value} exercendo a função de ${funcao.value} com o salário mensal de ${salario.value} trabalhou um total de ${diasTrabalhados.value} dias.`) alert para testar se está puxando os valores certos*/
 })
